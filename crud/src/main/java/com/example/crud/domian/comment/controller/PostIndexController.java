ccaca6bb8c5102f12db9b0b9055756e458f449d7
yusung103 <gimyuseong742@gmail.com>
2024-07-01 20:49:18 +0900

    modify

--- crud/src/main/java/com/example/crud/domian/comment/controller/PostIndexController.java
+++ crud/src/main/java/com/example/crud/domian/comment/controller/PostIndexController.java
@@ -7,8 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class PostIndexController {
-
-    private final CommentRepository commentRepository;
-    private final PostsRepository postsRepository;
+public class PostIndexController {\
 }
