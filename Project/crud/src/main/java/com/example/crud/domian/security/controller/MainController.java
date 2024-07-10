3197e9032d340a7187ab5991480e37fc4902466c
yusung103 <gimyuseong742@gmail.com>
2024-07-10 14:46:06 +0900

    remove JWT

--- Project/crud/src/main/java/com/example/crud/domian/security/controller/MainController.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud.domian.security.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MainController {
-
-}
--- Project/crud/src/main/java/com/example/crud/domian/user/entity/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.example.crud.domian.user.entity;
-
-public enum UserRole {
-    BRONZE, SILVER, GOLD, BLACKLIST, ADMIN;
-}
