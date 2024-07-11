2f52143c5b096fcfc84242e6faf6f1a6b4c17756
yusung103 <gimyuseong742@gmail.com>
2024-07-11 23:59:41 +0900

    add UserEntity

---
+++ Project/JWT/src/main/java/com/example/jwt/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.jwt.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String nickname;
+    public String password;
+}
