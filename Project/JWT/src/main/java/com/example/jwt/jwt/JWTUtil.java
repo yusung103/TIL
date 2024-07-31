d3dd27aae96604970eb4439399fbb2706f09eb76
yusung103 <gimyuseong742@gmail.com>
2024-07-31 23:43:24 +0900

    JWT

--- Project/JWT/src/main/java/com/example/jwt/jwt/JWTUtil.java
+++ Project/JWT/src/main/java/com/example/jwt/jwt/JWTUtil.java
@@ -1,9 +1,7 @@
 package com.example.jwt.jwt;
 
 import io.jsonwebtoken.Jwts;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
