d8d9f736995786256f7683c00dd22379b292d6e7
yusung103 <gimyuseong742@gmail.com>
2024-08-01 23:20:09 +0900

    JWT

---
+++ Project/JWT/src/main/java/com/example/jwt/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.example.jwt.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String MainP() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller : " + username + " " + role;
+    }
+}
