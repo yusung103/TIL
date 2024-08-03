2047fa321da9174e3041f731ac43ae25a10d036b
yusung103 <gimyuseong742@gmail.com>
2024-08-03 22:57:51 +0900

    .

--- Project/crud/src/main/java/com/example/crud/domian/board/dto/response/BoardListResponseDto.java
+++ Project/crud/src/main/java/com/example/crud/domian/board/dto/response/BoardListResponseDto.java
@@ -8,5 +8,4 @@ import lombok.Getter;
 public class BoardListResponseDto {
     private Long id;
     private String title;
-
 }
--- Project/crud/src/main/java/com/example/crud/domian/board/repository/BoardRepository.java
+++ Project/crud/src/main/java/com/example/crud/domian/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.example.crud.domian.board.repository;
 
 import com.example.crud.domian.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
