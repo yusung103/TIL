c9d43cedd74b2802f7141836e103b361179ab00f
yusung103 <gimyuseong742@gmail.com>
2024-08-08 20:19:59 +0900

    abstract

--- Kotlin/src/main/kotlin/Kotlin/클래스/Abstract.kt
+++ Kotlin/src/main/kotlin/Kotlin/클래스/Abstract.kt
@@ -1,2 +1,21 @@
 package Kotlin.클래스
 
+fun main() {
+    var r = Rabbit()
+
+    r.eat()
+    r.sniff()
+}
+
+abstract class Animal3() {
+    abstract fun eat()
+    fun sniff() {
+        println("킁킁")
+    }
+}
+
+class Rabbit : Animal3() {
+    override fun eat() {
+        println("당근을 먹습니다")
+    }
+}
